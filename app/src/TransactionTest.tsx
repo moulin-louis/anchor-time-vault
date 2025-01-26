9208d047981b534845fe60556021f7f8d993fbab
Louis Moulin <louis.moulin@outlook.fr>
2025-01-26 02:42:05 +0100

    comment some stuff for pipeline

--- app/src/TransactionTest.tsx
+++ app/src/TransactionTest.tsx
@@ -1,14 +1,14 @@
-import { useWallet } from '@solana/wallet-adapter-react';
-import { useState } from 'react';
-import { BN } from 'bn.js';
-import { useProgram } from './hooks';
+//import { useWallet } from '@solana/wallet-adapter-react';
+//import { useState } from 'react';
+//import { BN } from 'bn.js';
+//import { useProgram } from './hooks';
 
 
 export default function TransactionTest() {
-  const [count, setCount] = useState(new BN(0));
-  const [assignValue, setAssignValue] = useState('0'); // Declare a state variable...
-  const program = useProgram();
-  const wallet = useWallet();
+  //const [count, setCount] = useState(new BN(0));
+  //const [assignValue, setAssignValue] = useState('0'); // Declare a state variable...
+  //const program = useProgram();
+  //const wallet = useWallet();
 
   return (
     <div>
